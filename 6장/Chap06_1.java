0c64007bf7d6eedd5d7cad130940e8426a291f26
cg2z <97cg2z@gmail.com>
2019-06-15 20:41:49 +0900

    .

---
+++ 6장/Chap06_1.java
@@ -0,0 +1,25 @@
+class Mypoint{
+	private int x, y;
+	public Mypoint(int x, int y) { this.x=x; this.y=y;}
+	public boolean equals(Mypoint q) {
+		if (this.x==q.x&&this.y==q.y)
+			return true;
+		else return false;
+	}
+	public String toString() {
+		return ("Point("+x+","+y+")");
+	}
+}
+
+public class Chap06_1{
+	public static void main(String[] args)
+	{
+		Mypoint p = new Mypoint(3, 50);
+		Mypoint q = new Mypoint(4, 50);
+		System.out.println(p);
+		if(p.equals(q))
+			System.out.println("같은 점");
+		else
+			System.out.println("다른 점");
+	}
+}
